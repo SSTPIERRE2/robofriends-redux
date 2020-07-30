ebc9f059c0a58a90156dcce6664b1d3dc9fe755f
Steve <sstpierre2@partners.org>
2020-07-30 12:22:06 -0400

    shallow render for better test performance

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -15,13 +15,13 @@ describe("CounterButton", () => {
   });
 
   it("has an initial count of 1", () => {
-    const wrapper = mount(<CounterButton />);
+    const wrapper = shallow(<CounterButton />);
     expect(wrapper.state("count")).toBe(1);
   });
 
   it("increases count by 1 when clicked", () => {
     sandbox.spy(CounterButton.prototype, "shouldComponentUpdate");
-    const wrapper = mount(<CounterButton />);
+    const wrapper = shallow(<CounterButton />);
     wrapper.find("button").simulate("click");
     expect(wrapper.state("count")).toBe(2);
     expect(CounterButton.prototype.shouldComponentUpdate).toHaveProperty(
@@ -35,7 +35,7 @@ describe("CounterButton", () => {
 
   it("does not update if nextState's count is the same as the current state's count", () => {
     sandbox.spy(CounterButton.prototype, "shouldComponentUpdate");
-    const wrapper = mount(<CounterButton />);
+    const wrapper = shallow(<CounterButton />);
     wrapper.setState({ count: 1 });
     expect(CounterButton.prototype.shouldComponentUpdate).toHaveProperty(
       "callCount",
